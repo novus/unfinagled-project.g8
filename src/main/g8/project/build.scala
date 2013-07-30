81f2e40cad36d10d9e0278ea926e3b820299c74d
Chris Lewis <chris@thegodcode.net>
2013-07-29 23:32:29 -0400

    unfinagled version bump (0.2.0)

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -12,9 +12,9 @@ object Build extends sbt.Build {
             base = file("$name$-core"),
             settings = Seq(
               libraryDependencies ++= Seq(
-                "com.novus" %% "unfinagled-core" % "0.2.0-SNAPSHOT",
-                "com.novus" %% "unfinagled-server" % "0.2.0-SNAPSHOT",
-                "com.novus" %% "unfinagled-scalatest" % "0.2.0-SNAPSHOT" % "test"
+                "com.novus" %% "unfinagled-core" % "0.2.0",
+                "com.novus" %% "unfinagled-server" % "0.2.0",
+                "com.novus" %% "unfinagled-scalatest" % "0.2.0" % "test"
               )
             )
            )
